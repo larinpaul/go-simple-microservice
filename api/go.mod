d7c94b0afbdab20a62baa221c5c34b37c794563f
Pavel Larin <larinpavelfirst@mail.ru>
2023-09-26 11:33:12 -0300

    Starting a simple microservice

--- api/go.mod
+++ api/go.mod
@@ -1,3 +1,5 @@
 module api
 
 go 1.21.0
+
+require github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
--- api/main.go
+++ api/main.go
@@ -1,9 +1,33 @@
 package main
 
-// import (
-// 	"fmt"
-// 	"log"
-// 	"net/http"
-// 	"os"
-// 	"github.com/dgrijalva/jwt-go"
-// )
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	// "github.com/dgrijalva/jwt-go"
+)
+
+var MySigningKey = []byte(os.Getenv("SECRET_KEY"))
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Super Secret Information")
+}
+
+func isAuthorized(endpoint func(http.ResponseWriter, *http.Request)) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Header["Token"] != nil {
+
+		}
+	})
+}
+
+func handleRequests() {
+	http.Handle("/", isAuthorized(homePage))
+	log.Fatal(http.ListenAndServe(":9001", nil))
+}
+
+func main() {
+	fmt.Printf("server")
+	handleRequests()
+}
